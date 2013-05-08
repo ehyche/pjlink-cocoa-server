09267d1473f4abb497d0d3be3ccac48a7d4866de
Eric Hyche <ehyche@groupon.com>
2013-05-07 21:06:04 -0700

    Initial Commit

---
+++ PJLinkCocoaServer/PJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PJAppDelegate.h
+//  PJLinkCocoaServer
+//
+//  Created by Eric Hyche on 5/7/13.
+//  Copyright (c) 2013 Eric Hyche. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PJAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PJLinkCocoaServer/PJAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  PJAppDelegate.m
+//  PJLinkCocoaServer
+//
+//  Created by Eric Hyche on 5/7/13.
+//  Copyright (c) 2013 Eric Hyche. All rights reserved.
+//
+
+#import "PJAppDelegate.h"
+
+@implementation PJAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
